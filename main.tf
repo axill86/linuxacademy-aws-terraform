a863df0e2af9d1dee453be2788176c2f47489aa8
axill86 <An970217>
2019-12-05 07:57:48 +0300

    added storage and change formatting

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
 provider "aws" {
   region = "${var.aws_region}"
 }
+
 module "storage" {
-  source = "./storage"
+  source       = "./storage"
   project_name = "${var.project_name}"
+}
+
+module "networking" {
+  source = "./networking"
+  accessip = "${var.accessip}"
+  public_cidrs = "${var.public_cidrs}"
+  vpc_cidr = "${var.vpc_cidr}"
 }
--- storage/main.tf
+++ storage/main.tf
@@ -4,9 +4,10 @@ resource "random_id" "tf_bucket_id" {
 }
 
 resource "aws_s3_bucket" "tf_code" {
-  bucket = "${var.project_name}-${random_id.tf_bucket_id.dec}"
+  bucket        = "${var.project_name}-${random_id.tf_bucket_id.dec}"
   force_destroy = true
-  acl = "private"
+  acl           = "private"
+
   tags {
     Name = "tf_bucket"
   }
